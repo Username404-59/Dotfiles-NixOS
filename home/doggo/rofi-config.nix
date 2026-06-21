1bbf45fff96cdabccfd6a42fa61f3eb0c491da10
Username404-59 <w.iron.zombie@gmail.com>
2026-06-21 23:10:26 +0200

    Fix rofi config (by using home-manager)

--- home/doggo/rofi-config.nix
+++ home/doggo/rofi-config.nix
@@ -1,23 +1,22 @@
+{ ... }:
+
 {
-  home.file.".config/rofi/config.rasi".text = ''
-@import "catppuccin-mocha"
-@import "catppuccin-default"
+  programs.rofi = {
+    enable = true;
+    font = "mono 12";
+    location = "center";
 
-configuration {
-  modes: "run,ssh,drun";
-  font: "mono 12";
-  show-icons: true;
-  icon-theme: "Papirus Dark";
-
-  location: 0;
-  yoffset: 0;
-  xoffset: 0;
-  fixed-num-lines: true;
-
-  ml-row-down: "ScrollDown";                                                                                      
-  me-select-entry: "MousePrimary";                                                                                 
-  me-accept-entry: "MouseDPrimary";                                                                                
-  me-accept-custom: "Control+MouseDPrimary";
-}
-'';
+    extraConfig = {
+      modes = "run,ssh,drun";
+      "show-icons" = true;
+      "icon-theme" = "Papirus Dark";
+      yoffset = 0;
+      xoffset = 0;
+      "fixed-num-lines" = true;
+      "ml-row-down" = "ScrollDown";
+      "me-select-entry" = "MousePrimary";
+      "me-accept-entry" = "MouseDPrimary";
+      "me-accept-custom" = "Control+MouseDPrimary";
+    };
+  };
 }
