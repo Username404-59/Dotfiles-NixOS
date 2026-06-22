a294502300f57890855134a72ff920df9dffd7f1
Username404-59 <w.iron.zombie@gmail.com>
2026-06-22 05:05:05 +0200

    Only build necessary mesa drivers

--- modules/mesa.nix
+++ modules/mesa.nix
@@ -1,9 +1,19 @@
-{ pkgs, nixtamal, functions, ... }:
+{ lib, pkgs, functions, isLaptop, ... }:
 
+let
+  selected_vkDrivers = [ "amd" ] ++ lib.optional (!isLaptop) "intel";
+  selected_glDrivers = [ "radeonsi" "zink" /* Needed by steam => */ "d3d12" ] ++ lib.optional (!isLaptop) "i915";
+in
 {
   hardware.graphics = {
     # Mesa-git because why not
-    package = functions.mkUnstable pkgs.mesa;
-    package32 = functions.mkUnstable pkgs.pkgsi686Linux.mesa;
+    package = (functions.mkUnstable pkgs.mesa).override {
+      vulkanDrivers = selected_vkDrivers;
+      galliumDrivers = selected_glDrivers;
+    };
+    package32 = (functions.mkUnstable pkgs.pkgsi686Linux.mesa).override {
+      vulkanDrivers = selected_vkDrivers;
+      galliumDrivers = selected_glDrivers;
+    };
   };
 }
