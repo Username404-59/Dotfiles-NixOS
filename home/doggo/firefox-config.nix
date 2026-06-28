58a105236514bcb60e78439d0ea9a354228520ca
Username404-59 <w.iron.zombie@gmail.com>
2026-06-28 20:18:35 +0200

    Configure firefox more

--- home/doggo/firefox-config.nix
+++ home/doggo/firefox-config.nix
@@ -24,6 +24,38 @@ in
         extensions.force = true;
         userChrome = (builtins.readFile ./firefox_css/userChrome.css);
         userContent = (builtins.readFile ./firefox_css/userContent.css);
+
+        extensions.settings = {
+          "uBlock0@raymondhill.net".settings = {
+            selectedFilterLists = [ # https://github.com/gorhill/uBlock/blob/master/assets/assets.json
+              "ublock-filters"
+              "ublock-badware"
+              "ublock-privacy"
+              "ublock-unbreak"
+              "ublock-quick-fixes"
+              "ublock-experimental"
+              "easylist" "easyprivacy"
+
+              # Filter lists for cookie notices:
+              "adguard-cookies"
+              "ublock-cookies-adguard"
+              "fanboy-cookiemonster"
+              "ublock-cookies-easylist"
+
+              # Annoyances
+              "ublock-annoyances"
+              "adguard-popup-overlays"
+              "adguard-mobile-app-banners"
+              "adguard-other-annoyances"
+              "adguard-widgets"
+              "easylist-annoyances"
+              "easylist-chat"
+              "fanboy-ai-suggestions"
+              "easylist-newsletters"
+              "easylist-notifications"
+            ];
+          };
+        };
       };
     };
 
@@ -44,9 +76,19 @@ in
         "widget.use-xdg-desktop-portal.file-picker" = 1;
         "devtools.chrome.enabled" = true;
         "browser.newtabpage.activity-stream.widgets.enabled" = lock-false;
+        "browser.aboutConfig.showWarning" = lock-false;
 
+        # Performance etc
         "gfx.webrender.all" = lock-true;
         "layers.gpu-process.force-enabled" = lock-true;
+        "network.trr.mode" = 2; # Uses DNS over HTTPS which can maybe make my page loads faster
+      };
+
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
   };
