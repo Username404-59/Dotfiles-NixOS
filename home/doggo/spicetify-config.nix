a7a331f3086841df771cd4cc418ff22d6119bbde
Username404-59 <w.iron.zombie@gmail.com>
2026-06-29 16:42:03 +0200

    Use spotneotify spicetify theme

--- home/doggo/spicetify-config.nix
+++ home/doggo/spicetify-config.nix
@@ -1,11 +1,19 @@
-{ spicetify-nix, ... }:
+{ spicetify-nix, nixtamal, ... }:
 
 {
   programs.spicetify = {
     enable = true;
 
-    theme = spicetify-nix.packages.themes.catppuccin; # TODO: Use https://github.com/0lswitcher/spotneotify
-    colorScheme = "mocha";
+    theme = {
+      name = "spotneotify";
+      src = nixtamal.spotneotify;
+      injectCss      = true;
+      replaceColors  = true;
+      injectThemeJs  = false;
+      overwriteAssets = false;
+    };
+    colorScheme = "CatppuccinMocha";
+
     enabledExtensions = with spicetify-nix.packages.extensions; [];
     enabledCustomApps = with spicetify-nix.packages.apps; [];
   };
