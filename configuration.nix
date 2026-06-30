621e1673e780490d785c9755697ef6a96803a35b
Username404-59 <w.iron.zombie@gmail.com>
2026-06-30 18:44:37 +0200

    Set /bin/sh to dash

--- configuration.nix
+++ configuration.nix
@@ -151,6 +151,9 @@ in
   # To disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Replaces /bin/sh with dash (which is faster than bash)
+  environment.binsh = "${pkgs.dash}/bin/dash";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
