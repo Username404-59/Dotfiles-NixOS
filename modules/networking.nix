640b5b3629d9e7659a2c986cdb23d69690c9aa55
Username404-59 <w.iron.zombie@gmail.com>
2026-06-30 04:39:55 +0200

    Allow dev tests port

--- modules/networking.nix
+++ modules/networking.nix
@@ -30,10 +30,12 @@
         57621 # Spotify discovery port
         25565 # Minecraft
         7777 7778 27015 27016
+        8080 # Dev test port
       ];
       allowedUDPPorts = [
         5353 # Spotify discovery port
         7777 7778 27015 27016 # Satisfactory dedicated server (etc) ports
+        8080
       ];
     };
 
