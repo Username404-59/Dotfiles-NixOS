67073822462d2d076e3e4b4712aef9d554a9d661
Username404-59 <w.iron.zombie@gmail.com>
2026-06-30 19:20:07 +0200

    Use fish in kitty

--- home/doggo/doggo.nix
+++ home/doggo/doggo.nix
@@ -28,7 +28,6 @@ in
   ];
 
   home.packages = with pkgs; [
-    kitty
     kdePackages.kate
     (discord-canary.override {
       withOpenASAR = true;
--- home/doggo/kitty-config.nix
+++ home/doggo/kitty-config.nix
@@ -1,29 +1,45 @@
+{ ... }:
+
 {
-home.file.".config/kitty/kitty.conf".text = ''
-font_family Fira Code Regular
-bold_font auto
-italic_font auto
-bold_italic_font auto
+  programs.kitty = {
+    enable = true;
+    shellIntegration = {
+      mode = "enabled";
+      enableFishIntegration = true;
+    };
+    settings = {
+      shell = "fish";
+      font_family = "Fira Code Regular";
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
 
-cursor_shape beam
-disable_ligatures cursor
+      cursor_shape = "beam";
+      disable_ligatures = "cursor";
 
-font_size 12
+      font_size = 12;
 
-scrollback_lines 4096
-enable_audio_bell no
-copy_on_select clipboard
-strip_trailing_spaces smart
+      scrollback_lines = 4096;
+      enable_audio_bell = "no";
+      copy_on_select = "clipboard";
+      strip_trailing_spaces = "smart";
 
-sync_to_monitor no
-background_opacity 0.75
-initial_window_width 576
-initial_window_height 512
+      sync_to_monitor = "no";
+      background_opacity = 0.75;
+      initial_window_width = 576;
+      initial_window_height = 512;
 
-remember_window_size no
-window_padding_width 6
-placement_strategy center
-hide_window_decorations yes
-shell_integration enabled
-'';
+      remember_window_size = "no";
+      window_padding_width = 6;
+      placement_strategy = "center";
+      hide_window_decorations = "yes";
+    };
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disables greeting
+    '';
+  };
 }
