6bb445df3adf43b51cd200a3f9f231eeb8c09203
Username404-59 <w.iron.zombie@gmail.com>
2026-07-03 01:54:09 +0200

    Set default models in open-webui

--- common/ollama-config.nix
+++ common/ollama-config.nix
@@ -14,13 +14,16 @@
     enable = true;
     port = 6767;
     openFirewall = false;
-    environment = {
+    environment = rec {
       WEBUI_AUTH = "False"; # No need for auth since it's only accessible by me
       OFFLINE_MODE = "True"; # Update checks & model downloads aren't needed
       SAFE_MODE = "True";
       BYPASS_MODEL_ACCESS_CONTROL = "True";
       ENABLE_COMPRESSION_MIDDLEWARE = "False"; # Not needed since I access it via localhost
       OLLAMA_API_BASE_URL = "http://127.0.0.1:${toString config.services.ollama.port}/v1"; # Fixes qwen 3.6 as of writing; TODO Remove if not needed anymore
+
+      DEFAULT_MODELS = builtins.concatStringsSep ", " config.services.ollama.loadModels;
+      DEFAULT_PINNED_MODELS = DEFAULT_MODELS;
 
       DEFAULT_RAG_TEMPLATE = ''
 		### Task:
