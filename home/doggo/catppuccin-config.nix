755790d474e7f4bb7715ed50680d42a218caff1d
Username404-59 <w.iron.zombie@gmail.com>
2026-07-10 22:56:24 +0200

    Reformat catppuccin-godot comment

--- home/doggo/catppuccin-config.nix
+++ home/doggo/catppuccin-config.nix
@@ -32,13 +32,14 @@ in
 
   home.sessionVariables.GTK_THEME = shared_theme;
 
-  # Editor → Editor Settings → Text Editor → Theme
+  /*
+  Editor → Editor Settings → Text Editor → Theme
+  Editor → Editor Settings → Interface → Theme
+    Godot interface colors:
+      - Base Color: #1e1e2e
+      - Accent: #cba6f7
+      - Contrast: 0.2
+      - Icon Saturation: 0.6
+  */
   xdg.configFile."godot/text_editor_themes/Catppuccin Mocha.tet".source = nixtamal.catppuccin-godot; # Godot editor theme
-
-  # Editor → Editor Settings → Interface → Theme
-  # Godot interface colors:
-  # Base Color: #1e1e2e
-  # Accent: #cba6f7
-  # Contrast: 0.2
-  # Icon Saturation: 0.6
 }
