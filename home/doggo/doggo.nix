5280b62b5b6a345c4a0f1de8d471272da5e01111
Username404-59 <w.iron.zombie@gmail.com>
2026-07-12 01:39:43 +0200

    Better ani-cli TODO comment

--- home/doggo/doggo.nix
+++ home/doggo/doggo.nix
@@ -37,7 +37,7 @@
     kdePackages.kdenlive
     qalculate-qt
     mission-center
-    (functions.mkUnstable ani-cli) /* TODO Remove once the PR is merged -> */ botan3
+    (functions.mkUnstable ani-cli) /* TODO Remove once the PR is merged and the package has been updated -> */ botan3
     jetbrains.idea
     jetbrains.clion
     #androidStudioPackages.canary.full
