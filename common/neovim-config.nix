33af3979e8c8b19f99d2573c96a07ee9c7b8b15e
Username404-59 <w.iron.zombie@gmail.com>
2026-07-14 15:50:57 +0200

    Enable cmp plugin for completion in neovim

--- common/neovim-config.nix
+++ common/neovim-config.nix
@@ -11,6 +11,17 @@
     plugins = {
       transparent.enable = true;
       lualine.enable = true;
+      lsp.enable = true;
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+
+        settings.sources = [
+          { name = "nvim_lsp"; group_index = 1; }
+          { name = "path";     group_index = 1; }
+          { name = "buffer";   group_index = 2; max_item_count = 5; }
+        ];
+      };
     };
 
     globals = {
