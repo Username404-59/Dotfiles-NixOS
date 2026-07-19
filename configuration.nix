f12be44c4f236bd579c7a263de00c7f6be8a2eea
Username404-59 <w.iron.zombie@gmail.com>
2026-07-19 20:45:38 +0200

    Use git nix version

--- configuration.nix
+++ configuration.nix
@@ -92,6 +92,7 @@ in
   home-manager.useGlobalPkgs = false; # Home-manager inherits the pkgs path since NixOS 20.09 (unlike what the docs seem to say), meaning it uses my pinned nixpkgs source already
   nix.settings.auto-optimise-store = true;
   nix.settings.experimental-features = [ "nix-command" "blake3-hashes" ]; # blake3 is for nixtamal
+  nix.package = pkgs.nixVersions.git;
 
   catppuccin = {
     autoEnable = true;
