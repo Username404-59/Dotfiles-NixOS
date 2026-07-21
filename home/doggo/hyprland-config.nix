9372f5d42c68340fca33adc1e6b519b89a3041c0
Username404-59 <w.iron.zombie@gmail.com>
2026-07-21 02:41:06 +0200

    Make background transparent in libreoffice suite

--- home/doggo/hyprland-config.nix
+++ home/doggo/hyprland-config.nix
@@ -389,6 +389,7 @@ in
             "org.prismlauncher.PrismLauncher"
             "com.obsproject.Studio"
             "${config.programs.chromium.package.pname}-browser"
+            "libreoffice-.*"
             "org.kde.*|qt.*|.*qt.*|.*Qt.*"
           ]})$";
 
