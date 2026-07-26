e39546cd8f9dc49cfc5c20f8a0ffc2d0df6a3271
Username404-59 <w.iron.zombie@gmail.com>
2026-07-26 21:14:55 +0200

    Fix iso-installer

--- ISO/iso.nix
+++ ISO/iso.nix
@@ -1,8 +1,11 @@
 { lib, ... }:
 
 {
-  boot.supportedFilesystems = {
-    f2fs = true;
-    zfs = lib.mkForce false; # Pulled by ISO build for some reason, and it's broken as of writing
+  image.modules.iso-installer = {
+    boot.supportedFilesystems = {
+      f2fs = true;
+      zfs = lib.mkForce false; # Pulled by ISO build for some reason, and it's broken as of writing
+    };
+    boot.zswap.enable = lib.mkForce false;
   };
 }
