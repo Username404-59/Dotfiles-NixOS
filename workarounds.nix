c4f145ef59859156ead35e77cb454e2e5c98e925
Username404-59 <w.iron.zombie@gmail.com>
2026-07-26 19:53:10 +0200

    Move immutable /etc workarounds to workarounds.nix (except coolercontrol)

--- workarounds.nix
+++ workarounds.nix
@@ -2,4 +2,15 @@
 
 {
   documentation.nixos.enable = false; # In case something's documentation is making nixos-rebuild fail
+
+  # Fixes for services broken by system.etc.overlay.mutable (from modules/filesystems.nix)
+  systemd.tmpfiles.rules = [
+    "d /var/lib/ssh 0755 root root -"
+  ];
+  services.openssh.hostKeys = [
+    {
+      path = "/var/lib/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }
+  ];
 }
