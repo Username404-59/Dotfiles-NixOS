96cc88ef69c8c9a30ae6396c4593b5e179dcf193
Username404-59 <w.iron.zombie@gmail.com>
2026-08-04 01:04:56 +0200

    Add workaround for NetworkManager wifi connections

--- workarounds.nix
+++ workarounds.nix
@@ -1,19 +1,28 @@
 { ... }:
 
+let
+  dirs = {
+    ssh = "/var/lib/ssh";
+    lact = "/var/lib/lact";
+    nw_manager = "/var/lib/NetworkManager/system-connections";
+  };
+in
 {
   documentation.nixos.enable = false; # In case something's documentation is making nixos-rebuild fail
 
   # Fixes for services broken by system.etc.overlay.mutable (from modules/filesystems.nix)
   systemd.tmpfiles.rules = [
-    "d /var/lib/ssh 0755 root root -"
-    "d /var/lib/lact 0755 root root -"
+    "d ${dirs.ssh} 0755 root root -"
+    "d ${dirs.lact} 0755 root root -"
+    "d ${dirs.nw_manager} 0700 root root -"
   ];
-  environment.etc."lact".source = "/var/lib/lact";
+  environment.etc."lact".source = dirs.lact;
   services.openssh.hostKeys = [
     {
-      path = "/var/lib/ssh/ssh_host_ed25519_key";
+      path = "${dirs.ssh}/ssh_host_ed25519_key";
       type = "ed25519";
     }
   ];
   environment.etc."avahi/services/.keep".text = ""; # https://github.com/nixos/nixpkgs/issues/539763
+  networking.networkmanager.settings.keyfile.path = dirs.nw_manager;
 }
