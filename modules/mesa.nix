4ff4bdc2267db6a9578a84f26d89f256bb851bbd
Username404-59 <w.iron.zombie@gmail.com>
2026-08-07 16:34:07 +0200

    Set RADV_PERFTEST="nogttspill" on desktop

--- modules/mesa.nix
+++ modules/mesa.nix
@@ -19,4 +19,7 @@ in
       (functions.mkUnstable low-latency-layer) # Better alternative (+ vendor-agnostic) to mesa's amd anti-lag 2
     ];
   };
+
+  # https://docs.mesa3d.org/envvars.html#:~:text=RADV%5FPERFTEST
+  environment.sessionVariables.RADV_PERFTEST = lib.mkIf (!isLaptop) "nogttspill";
 }
