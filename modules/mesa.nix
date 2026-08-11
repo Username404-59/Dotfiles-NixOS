d25d3a1568d8ae5ef57c056c7c3d155d39cc504e
Username404-59 <w.iron.zombie@gmail.com>
2026-08-11 21:15:44 +0200

    Stop using mesa-git

--- modules/mesa.nix
+++ modules/mesa.nix
@@ -1,20 +1,7 @@
 { lib, pkgs, functions, isLaptop, ... }:
 
-let
-  selected_vkDrivers = [ "amd" "nouveau" /* Needed by steam => */ "microsoft-experimental" ] ++ lib.optional (!isLaptop) "intel";
-  selected_glDrivers = [ "radeonsi" "zink" ] ++ lib.optional (!isLaptop) "i915"; # TODO Use xe -> iris instead of i915 once https://gitlab.freedesktop.org/drm/xe/kernel/-/merge_requests/361 is merged
-in
 {
   hardware.graphics = {
-    # Mesa-git because why not
-    package = (functions.mkUnstable pkgs.mesa).override {
-      vulkanDrivers = selected_vkDrivers;
-      galliumDrivers = selected_glDrivers;
-    };
-    package32 = (functions.mkUnstable pkgs.pkgsi686Linux.mesa).override {
-      vulkanDrivers = selected_vkDrivers;
-      galliumDrivers = selected_glDrivers;
-    };
     extraPackages = with pkgs; [
       (functions.mkUnstable low-latency-layer) # Better alternative (+ vendor-agnostic) to mesa's amd anti-lag 2
     ];
