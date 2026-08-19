933e726186ef6d4e519f39e65ba79dcfd313edac
Username404-59 <w.iron.zombie@gmail.com>
2026-08-19 22:01:16 +0200

    Add major_version variable in ableton-config.nix

--- home/doggo/ableton-config.nix
+++ home/doggo/ableton-config.nix
@@ -10,12 +10,13 @@ in
 
   # Hacky activation script to install ableton
   home.activation.ableton = let
+    major_version = "12";
     tmp_dir = "$XDG_RUNTIME_DIR/abletonstuff";
   in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -e "${config.home.homeDirectory}/.wine-ableton" ]; then
       rm -rf ${tmp_dir}
       mkdir ${tmp_dir}
-      ln -sf ${nixtamal.ableton} ${tmp_dir}/ableton_live_lite_12.9999.9999_64.zip
+      ln -sf ${nixtamal.ableton} ${tmp_dir}/ableton_live_lite_${major_version}.9999.9999_64.zip
       PATH="/run/current-system/sw/bin:${pkgs.unzip}/bin:${pkgs.cabextract}/bin:$PATH" ABLETON_INSTALLER_DIR="${tmp_dir}" ABLETON_LIVE_AUTOINSTALL=1 ABLETON_INSTALLER_UI=0 ${ableton.apps.${pkgs.stdenv.hostPlatform.system}.setup-prefix.program}
       rm -rf ${tmp_dir}
     fi
